8d02cf3d3673c070624b42c6f12bffaaccd75557
nevena cukucan <nenacukucan@yahoo.com>
2019-09-11 23:31:12 +0200

    clean project

---
+++ main.cs
@@ -0,0 +1,17 @@
+﻿using NUnit;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using System;
+
+
+namespace VSC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            
+        }
+    }
+}
