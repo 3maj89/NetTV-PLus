43ae1ef690ac524914795520d5382a522c178bf5
nevena cukucan <nenacukucan@yahoo.com>
2019-09-17 20:12:23 +0200

    ispravljen typo u using

--- Test Run/TestRunner.cs
+++ Test Run/TestRunner.cs
@@ -1,9 +1,6 @@
-husing NUnit.Framework;
+using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
-using OpenQA.Selenium.Firefox;
-using OpenQA.Selenium.Support;
-using System;
 
 namespace TestRun
 {
